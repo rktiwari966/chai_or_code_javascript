169a1398c36716c23d9a23fb1b416b58a5ca156f
Ritin Tiwari <102997855+rktiwari966@users.noreply.github.com>
2024-09-04 17:37:41 +0000

    Variables In Javascript

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "hiteshchoudhary@gmail.com"
+var accountCity = "Jaipur"
+accountPassword = 1234
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountCity);
+console.log(accountPassword);
+
+// accountId = 11234; Not Allowed
