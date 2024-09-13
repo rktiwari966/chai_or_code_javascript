6fe5f5f3a154f8c7647178b76fc539f97f6707dd
Ritin <tiwariritin469@gmail.com>
2024-09-13 23:43:05 +0530

    Variables and datatypes in javascript

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,10 +2,21 @@ const accountId = 144553
 let accountEmail = "hiteshchoudhary@gmail.com"
 var accountCity = "Jaipur"
 accountPassword = 1234
+let accountState;
 
-console.log(accountId);
-console.log(accountEmail);
-console.log(accountCity);
-console.log(accountPassword);
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountCity);
+// console.log(accountPassword);
+
+
+console.table([accountId,accountEmail,accountCity,accountPassword]);
+
+accountEmail = "hc@gmail.com";
+accountPassword = "212121";
+accountyCity = "Jabalpur";
+
+console.table([accountEmail,accountPassword,accountCity,accountState]);
+
 
 // accountId = 11234; Not Allowed
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// alert("Hello world"); we are using nodejs not browser
+
+let Name = "ritin tiwari";
+let age = 24;
+let isLoggedIn = false;
+
+console.table([Name,age,isLoggedIn]);
+
+console.log(typeof("Hitesh Chaudhary"));
+console.log(typeof(null));
